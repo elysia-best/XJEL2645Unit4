f43aaabc8d986fa39038c225f27db79adc13b557
Yinan <ghayinanoghaqino@icloud.com>
2024-03-31 20:12:29 +0800

    feat: Add CMSIS Math library.

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -1,6 +1,6 @@
 /*
  * (C) Copyright 2024 Yuanzhen Gan. All Rights Reserved.
- * Description: main.cpp   
+ * Description: main.cpp
  * Author: Yuanzhen Gan
  * Date: 2024-03-31
  * Modify Record:
@@ -10,12 +10,20 @@
 
 #include <mbed.h>
 
+#include "arm_math.h"
 
 /**
-  * @Name    main
-  * @brief  Entry point for the app
-  **/
+ * @Name    main
+ * @brief  Entry point for the app
+ **/
 int main() {
+  int16_t i;
+  float32_t pSinVal;
+  float32_t pCosVal;
+  for (i = -180; i < 180; i++) {
+    arm_sin_cos_f32(i, &pSinVal, &pCosVal);
+    printf("%d,%d\n", (int)pSinVal, (int)pCosVal);
+  }
   while (true) {
   }
 }
