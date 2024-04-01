f2d0d17f0b3187031082b74ff20947d7ff5f2545
Yinan <ghayinanoghaqino@icloud.com>
2024-04-01 23:36:28 +0800

    feat: New file: GameEngine

---
+++ Core/Inc/GameEngine.h
@@ -0,0 +1,15 @@
+/*
+ * (C) Copyright 2024 Yuanzhen Gan. All Rights Reserved.
+ * Description: GameEngine.h   
+ * Author: Yuanzhen Gan
+ * Date: 2024-04-01
+ * Modify Record:
+ */
+
+#ifndef __GAME_ENGINE_H_
+#define __GAME_ENGINE_H_
+
+
+#endif
+
+
---
+++ Core/Src/GameEngine.cpp
@@ -0,0 +1,9 @@
+/*
+ * (C) Copyright 2024 Yuanzhen Gan. All Rights Reserved.
+ * Description: GameEngine.cpp   
+ * Author: Yuanzhen Gan
+ * Date: 2024-04-01
+ * Modify Record:
+ */
+
+#include "GameEngine.h"
