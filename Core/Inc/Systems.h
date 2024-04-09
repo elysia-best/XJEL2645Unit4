861b9f47c7af535535c6a2022a0a815db1a95822
Yinan <ghayinanoghaqino@icloud.com>
2024-04-10 00:20:53 +0800

    Added Systems

---
+++ Core/Inc/Systems.h
@@ -0,0 +1,17 @@
+/*
+ * (C) Copyright 2024 Yuanzhen Gan. All Rights Reserved.
+ * Description: Systems.h
+ * Author: Yuanzhen Gan
+ * Date: 2024-04-09
+ * Modify Record:
+ */
+#ifndef _SYSTEMS_H_
+#define _SYSTEMS_H_
+
+#include "pico_ecs.h"
+
+namespace Systems {
+extern ecs_id_t TRANSFORM_SYSTEM;
+}
+
+#endif
---
+++ Core/Src/Systems.cpp
@@ -0,0 +1,8 @@
+/*
+ * (C) Copyright 2024 Yuanzhen Gan. All Rights Reserved.
+ * Description: Systems.cpp
+ * Author: Yuanzhen Gan
+ * Date: 2024-04-09
+ * Modify Record:
+ */
+#include "Systems.h"
