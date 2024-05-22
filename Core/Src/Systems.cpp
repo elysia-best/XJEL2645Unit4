18fe12094c885d1476720cc50a1e5150c8f7c4b0
Elysia <c.elysia@foxmail.com>
2024-05-22 23:19:04 +0800

    Update

--- Core/Src/Systems.cpp
+++ Core/Src/Systems.cpp
@@ -292,8 +292,8 @@ void Systems::GameControlSystem::m_startLevel() {
   }
 
   // Generate position for this play
-  std::random_device rd;  //如果可用的话，从一个随机数发生器上获得一个真正的随机数
-  std::mt19937 gen(rd()); //gen是一个使用rd()作种子初始化的标准梅森旋转算法的随机数发生器
+  std::random_device rd;  
+  std::mt19937 gen(rd()); 
   std::uniform_int_distribution<> distrib(0, 3);
 
   const int RAND_NUM = 4;
